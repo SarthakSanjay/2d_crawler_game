9f0fdd2ef8da9d414b1434b4a4ccb855492546c7
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-11-16 23:41:52 +0530

    refactore game scene

--- src/game/scenes/Game.ts
+++ src/game/scenes/Game.ts
@@ -4,23 +4,25 @@ import { debugDraw } from "../utils/debug";
 import { createLizardAnims } from "../anims/EnemyAnims";
 import { createCharaterAnims } from "../anims/CharacterAnims";
 import Lizard from "../enemies/Lizard";
+import '../character/Faune'
+import Faune from "../character/Faune";
 
 export class Game extends Scene {
     camera: Phaser.Cameras.Scene2D.Camera;
     background: Phaser.GameObjects.Image;
     gameText: Phaser.GameObjects.Text;
-    cursor: Phaser.Types.Input.Keyboard.CursorKeys;
-    faune: Phaser.Physics.Arcade.Sprite;
+    cursors: Phaser.Types.Input.Keyboard.CursorKeys;
+    faune: Faune;
     lizard: Phaser.Physics.Arcade.Sprite;
     wallLayer!: Phaser.Tilemaps.TilemapLayer;
 
-    private hit = 0
     constructor() {
         super("Game");
     }
 
     preload() {
-        this.cursor = this.input.keyboard?.createCursorKeys()
+        //@ts-ignore
+        this.cursors = this.input.keyboard?.createCursorKeys()
     }
 
     create() {
@@ -36,16 +38,14 @@ export class Game extends Scene {
         //@ts-ignore
         map.createLayer("ground", tileset);
 
+
+        //@ts-ignore
         this.wallLayer = map.createLayer("walls", tileset);
         this.wallLayer?.setCollisionByProperty({ collides: true })
 
         // debugDraw(this.wallLayer, this)
 
-        this.faune = this.physics.add.sprite(128, 128, 'faune', 'walk-down-3.png')
-        this.faune.body?.setSize(this.faune.width * 0.5, this.faune.height * 0.8)
-
-
-        this.faune.anims.play('faune-idle-down')
+        this.faune = this.add.faune(128, 128, 'faune')
 
         this.camera.startFollow(this.faune, true)
 
@@ -53,11 +53,14 @@ export class Game extends Scene {
             classType: Lizard,
             createCallback: (go) => {
                 const lizGo = go as Lizard
+
+                //@ts-ignore
                 lizGo.body.onCollide = true
             }
         })
 
         lizards.get(255, 128, 'lizard')
+        lizards.get(300, 300, 'lizard')
 
 
         this.physics.add.collider(this.faune, this.wallLayer)
@@ -66,6 +69,7 @@ export class Game extends Scene {
         this.physics.add.collider(
             lizards,
             this.faune,
+            //@ts-ignore
             this.handlePlayerLizardCollison,
             undefined,
             this
@@ -80,47 +84,14 @@ export class Game extends Scene {
         const dy = this.faune.y - lizard.y
 
         const dir = new Phaser.Math.Vector2(dx, dy).normalize().scale(200)
-        this.faune.setVelocity(dir.x, dir.y)
 
-        this.hit = 1
+        this.faune.handleDamage(dir)
 
     }
 
     update(time: number, delta: number): void {
-        if (this.hit > 0) {
-            ++this.hit
-            if (this.hit > 10) {
-                this.hit = 0
-            }
-            return
-        }
-        if (!this.cursor || !this.faune) return
-
-        const speed = 100
-        if (this.cursor.left.isDown) {
-            this.faune.anims.play('faune-run-side', true)
-            this.faune.setVelocity(-speed, 0)
-            this.faune.scaleX = -1
-            this.faune.body?.setOffset(24, 0)
-        } else if (this.cursor.right.isDown) {
-            this.faune.setVelocity(speed, 0)
-            this.faune.anims.play('faune-run-side', true)
-            this.faune.body?.setOffset(8, 0)
-            this.faune.scaleX = 1
-        } else if (this.cursor.up.isDown) {
-            this.faune.anims.play('faune-run-up', true)
-            this.faune.setVelocity(0, -speed)
-
-        } else if (this.cursor.down.isDown) {
-            this.faune.setVelocity(0, speed)
-            this.faune.anims.play('faune-run-down', true)
-        }
-
-        else {
-            const parts = this.faune.anims.currentAnim?.key.split('-')
-            parts[1] = 'idle'
-            this.faune.anims.play(parts?.join('-'))
-            this.faune.setVelocity(0, 0)
+        if (this.faune) {
+            this.faune.update(this.cursors)
         }
     }
 
